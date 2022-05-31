5e46f4e524871e0eac8b3a3ef376c75ab79e0af3
TheTUFGuy <ppmall20@iitk.ac.in>
2022-05-31 15:28:58 +0530

    Add Assignment1

---
+++ Assignment1/README.md
@@ -0,0 +1,15 @@
+## DETAILS
+
+<ul> 
+    <li> Program implemented in Python 3.9.13
+    <li> hashlibrary function of python was used
+    <li> To run program in terminal do > python3 task1.py
+
+</ul>
+
+## EXAMPLE
+
+> Enter a String :iitk </br>
+nonce value =  184519 </br>
+Time Taken = 0.9494516849517822 </br>
+Corresponding Hash =  0000e11b0268ff370bf302c8db25888f04fe1569cd20e5cac10b21d3b7db6413</br>
---
+++ Assignment1/task1.py
@@ -0,0 +1,19 @@
+import hashlib
+from string import hexdigits
+import time
+target = "0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+string = input("Enter a String :")
+start = time.time()
+i = 0
+string1 = ""
+while i < 9999999:
+    string1 = string + str(i)
+    encoded = string1.encode()
+    result = (hashlib.sha256(encoded)).hexdigest()
+    if(result <= target):
+        # print(string1, encoded)
+        timetaken = time.time()-start
+        print("nonce value = ", i, "\nTime Taken =",
+              timetaken, "\nCorresponding Hash = ", result)
+        break
+    i += 1
